9b97a58e47548f18a193d3670cd9df299101d09f
vandamd <vandamdinh71@gmail.com>
2022-12-21 16:33:28 +0000

    Updated first script

--- 1_baseimage.sh
+++ 1_baseimage.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 docker run -it \
-    --name macos_base \
     --device /dev/kvm \
     -p 50922:10022 \
     -p 5999:5999 \
-    -e NOPICKER=true \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e "DISPLAY=${DISPLAY:-:0.0}" \
     -e EXTRA="-display none -vnc 0.0.0.0:99" \
